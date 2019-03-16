3148bce660b887e196ea0b71a2e1e10c6d484371
Help <pczerokey@gmail.com>
2019-03-16 00:43:25 -0400

    Error cleanup, everything works

--- src/components/GarageInfo.js
+++ src/components/GarageInfo.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import './GarageInfo.css';
-import {Row,Card, CardText, CardBody,
+import {Row,Card, CardBody,
   CardTitle, Col } from 'reactstrap';
 
 class App extends Component 
@@ -19,14 +19,12 @@ class App extends Component
               <Card>
                 <CardBody>
                 <CardTitle>{this.props.garageName}</CardTitle>
-                <CardText>
                     <ul>
                       <li>Student Spots Open: {this.props.spotsAvailableForStudents}</li>
                       <li>Student Parking Spots Taken: {this.props.percentFullStudents}</li>
                       <li>Employee Spots Open: {this.props.spotsAvailableForEmployees}</li>
                       <li>Employee Parking Spots Taken: {this.props.percentFullEmployees}</li>
                   </ul>
-                  </CardText>
                   </CardBody>
                 </Card>
             </Col>
